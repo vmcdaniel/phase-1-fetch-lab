59c29345838d06b30d8bf5c09ea0b929683c104c
Victoria <vmcdaniel459@gmail.com>
2024-12-09 16:16:56 -0500

    great job

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then (json => {
+    console.log(json)
+    return json;
+  });
 }
 
-function renderBooks(books) {
+
+  // To pass the tests, don't forget to return your fetch!
+  function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
-}
+    })
+  };
+
+  
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks().then(books => renderBooks(books));
 });
